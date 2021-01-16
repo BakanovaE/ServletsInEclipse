818fc1506cade99cf16c8baf412be30163b51033
Ekaterina Bakanova <k125598@yandex.ru>
2021-01-16 13:16:56 +0300

    Get request from html form

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -1,33 +1,46 @@
-
-
-import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
+  
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter writer = response.getWriter();
 		
-		String[] nums = request.getParameterValues("nums");
-		
-		try {
-			writer.println("<h2>Nums: ");
-			for (String n : nums)
-				writer.println(n + " ");
-			writer.println("</h2");
-		} finally {
-			writer.close();
-		}
+	
+		writer.println("Hello Servlet");
+		writer.close();
 	}
-
+  
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+          
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+ 
+        String name = request.getParameter("username");
+        String age = request.getParameter("userage");
+        String gender = request.getParameter("gender");
+        String country = request.getParameter("country");
+        String[] courses = request.getParameterValues("courses");
+         
+        try {
+            writer.println("<p>Name: " + name + "</p>");
+            writer.println("<p>Age: " + age + "</p>");
+            writer.println("<p>Gender: " + gender + "</p>");
+            writer.println("<p>Country: " + country + "</p>");
+            writer.println("<h4>Courses</h4>");
+            for(String course: courses)
+                writer.println("<li>" + course + "</li>");
+        } finally {
+            writer.close();  
+        }
+    }
 }
