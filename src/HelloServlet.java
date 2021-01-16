f57b15cb3788f429edebb8decddc087e0a285407
Ekaterina Bakanova <k125598@yandex.ru>
2021-01-16 14:29:30 +0300

    Redirect

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -1,6 +1,6 @@
-import java.io.PrintWriter;
 import java.io.IOException;
-
+import java.io.PrintWriter;
+ 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -11,19 +11,26 @@ import javax.servlet.http.HttpServletResponse;
   
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-	
-
+  
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-          
-        //String path = "/index.html";
-        //ServletContext servletContext = getServletContext();
-        //RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
-        //requestDispatcher.forward(request, response);
         
-        String path = "/notfound";
-        ServletContext servletContext = getServletContext();
-        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
-        requestDispatcher.forward(request, response);
+    	String id = request.getParameter("id");
+        
+    	if(id == null) {
+             
+            String path = request.getContextPath() + "/notfound";
+            
+            response.sendRedirect(path);
+        }
+        else {
+            response.setContentType("text/html");
+            
+            PrintWriter writer = response.getWriter();
+            
+            writer.println("<h2>Hello Id " + id + "</h2>");
+            
+            writer.close();  
+        }
     }
 }
